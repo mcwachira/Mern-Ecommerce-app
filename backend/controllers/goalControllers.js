9f55954496d0e71d53fd44a2a393b2efd5097293
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-11-03 02:10:19 +0300

    added a model and mongodb
    created a goal model and connected the backend  to the mongodb  database to store my data

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler')
+const Goals = require('../models/goalModel')
 
 //@desc  get  goals
 //@route GET  /api/goals
 //@access Public
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'read goals' })
+    const goals = await Goals.find()
+    res.status(200).send(goals)
 })
 
 //@desc  create a   goal
@@ -13,9 +15,14 @@ const getGoals = asyncHandler(async (req, res) => {
 //@access Private
 
 const createGoals = asyncHandler(async(req, res) => {
-    if(!req.body.text){
+    if(!req.body){
         throw new Error('please add text')
     }
+    console.log(req.body)
+
+    const {name, description} = req.body
+    const newGoal = await new Goals({name, description}).save()
+    res.status(200).json(newGoal)
 })
 
 
@@ -25,7 +32,17 @@ const createGoals = asyncHandler(async(req, res) => {
 //@access Private
 
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update goals  ${req.params.id}` })
+   const goal = await Goals.findById(req.params.id)
+   if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+    
+   }
+
+   const updatedGoal =await Goals.findByIdAndUpdate(req.params.id,req.body,{
+      new:true,
+   } )
+    res.status(200).json(updatedGoal)
 })
 
 
@@ -35,7 +52,16 @@ const updateGoals = asyncHandler(async (req, res) => {
 //@access Private
 
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete goals  ${req.params.id}` })
+    const goal = await Goals.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+
+    }
+
+   // const newGoals = await Goals.findByIdAndDelete(req.params.id)
+   await goal,remove()
+    res.status(200).json(newGoals)
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const dotEnv = require('dotenv').config()
+const dotEnv = require('dotenv').config();
+const colors = require('colors')
 const {errorHandler}   = require('./middleware/errorMiddleware')
-
+const connectDb = require('./config/db')
 const app = express()
 
 const PORT = process.env.PORT || 5000;
 
-
+connectDb()
 
 //to get values from our body
 
