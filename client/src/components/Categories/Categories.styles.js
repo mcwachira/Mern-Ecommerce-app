83aab7459ba0d3d93b77403ddd9909821d30fe55
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-06-23 08:45:26 +0300

    updating the app to a new design

--- client/src/components/Categories/Categories.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-import { mobile } from '../../utils/responsive'
-export const Container = styled.div`
-display:flex;
-justify-content:space-between;
-align-items:center;
-    ${mobile({ padding: '0px', flexDirection: 'column' })}
-`
--- client/src/components/Products/Products.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`
